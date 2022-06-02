43c581ca0c46be3117a0f74990aa4818c384a22b
Ranto <rantoirwanda08@gmail.com>
2022-06-03 02:31:37 +0700

    ui -login

--- vaccinebooking/src/pages/Login.jsx
+++ vaccinebooking/src/pages/Login.jsx
@@ -26,8 +26,7 @@ const Login = () => {
     }
   return (
     <>
-            <Container >
-                
+            {/* <Container className='mt-10' >
                     <Row className="mt-5" >
                     <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
                         <Form onSubmit={Auth}>
@@ -47,7 +46,38 @@ const Login = () => {
                         </Form>
                     </Col>
                 </Row>
-            </Container>
+            </Container> */}
+            <div className="container">
+    <div className="row">
+      <div className="col-md-6 offset-md-3">
+        <h2 className="text-center text-dark mt-5">Login Form</h2>
+        <div className="text-center mb-5 text-dark">Made with bootstrap</div>
+        <div className="card my-5">
+
+          <form className="card-body cardbody-color p-lg-5" onSubmit={Auth}>
+
+            <div className="text-center">
+              <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png" className="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                width="200px" alt="profile"/>
+            </div>
+
+            <div className="mb-3">
+              <input type="text" className="form-control" id="Username" aria-describedby="emailHelp" placeholder="User Name" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+            </div>
+            <div className="mb-3">
+              <input type="password" className="form-control" id="password" placeholder="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+            </div>
+            <div className="text-center"><button type="submit" className="btn btn-color px-5 mb-5 w-100">Login</button></div>
+            <div id="emailHelp" className="form-text text-center mb-5 text-dark">Not
+              Registered? <a href="#" className="text-dark fw-bold"> Create an
+                Account</a>
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
         </>
   )
 }
