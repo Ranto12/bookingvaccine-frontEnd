05ac4de2a010eaa269e0aca44b89af220dd06547
wandy17 <79315678+wandy17@users.noreply.github.com>
2022-06-09 20:15:50 +0800

    update

--- vaccinebooking/src/apollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-    uri: 'https://prepared-midge-65.hasura.app/v1/graphql',
-    cache: new InMemoryCache(),
-});
-
-export default client;
