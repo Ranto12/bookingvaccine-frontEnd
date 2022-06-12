acf79eca45f203245ddbeb9d8ffe539e72e5fb83
wandy17 <79315678+wandy17@users.noreply.github.com>
2022-06-12 19:55:42 +0800

    update

--- vaccinebooking/src/App.js
+++ vaccinebooking/src/App.js
@@ -7,7 +7,7 @@ import DataBooking from "./pages/DataBooking/DataBooking";
 import KelolaPengguna from "./pages/KelolaPengguna/KelolaPengguna";
 import KelolaJadwal from "./pages/KelolaJadwal/KelolaJadwal";
 import KelolaAdmin from "./pages/KelolaAdmin/KelolaAdmin";
-import JadwalVaksinasi from "./pages/KelolaJadwal/JadwalVaksinasi";
+import JadwalVaksinasi from "./pages/KelolaJadwal/jadwalVaksinasi";
 // import ViewUser from './domain/veiwUser/ViewUser';
 // import Login from './pages/Login/Login';
 
@@ -20,7 +20,7 @@ function App() {
       <Route path="/DataBooking" element={<DataBooking />} />
       <Route path="/KelolaPengguna" element={<KelolaPengguna />} />
       <Route path="/jadwalvaksinasi" element={<JadwalVaksinasi />} />
-      <Route path='/login' element={<Login/>}/>
+      <Route path='/login' element={<Login />} />
       <Route path="/KelolaJadwal" element={<KelolaJadwal />} />
       <Route path="/KelolaAdmin" element={<KelolaAdmin />} />
       {/* <Route path='/login' element={<ViewUser/>}/> */}
