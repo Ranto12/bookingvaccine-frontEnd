70bbb0c631a67f4a2c281c63e759bea0a7c8d02a
Ranto <rantoirwanda08@gmail.com>
2022-06-12 13:33:05 +0700

    cek 1

--- vaccinebooking/src/App.js
+++ vaccinebooking/src/App.js
@@ -4,8 +4,14 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dasboard/Dasboard";
 import KelolaBerita from "./pages/KelolaBerita/KelolaBerita";
 import DataBooking from "./pages/DataBooking/DataBooking";
+import KelolaPengguna from "./pages/KelolaPengguna/KelolaPengguna";
 import KelolaJadwal from "./pages/KelolaJadwal/KelolaJadwal";
 import KelolaAdmin from "./pages/KelolaAdmin/KelolaAdmin";
+import JadwalVaksinasi from "./pages/KelolaJadwal/JadwalVaksinasi";
+import AddAdmin from "./pages/KelolaAdmin/AddAdmin";
+// import ViewUser from './domain/veiwUser/ViewUser';
+// import Login from './pages/Login/Login';
+import ArtikelTerbaru from "./pages/ArtikelTerbaru/ArtikelTerbaru";
 
 function App() {
   return (
@@ -14,8 +20,14 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/KelolaBerita" element={<KelolaBerita />} />
       <Route path="/DataBooking" element={<DataBooking />} />
+      <Route path="/KelolaPengguna" element={<KelolaPengguna />} />
+      <Route path="/jadwalvaksinasi" element={<JadwalVaksinasi />} />
+      <Route path='/login' element={<Login/>}/>
       <Route path="/KelolaJadwal" element={<KelolaJadwal />} />
       <Route path="/KelolaAdmin" element={<KelolaAdmin />} />
+      <Route path="/ArtikelTerbaru" element={<ArtikelTerbaru />} />
+      <Route path="/AddAdmin" element={<AddAdmin />} />
+
       {/* <Route path='/login' element={<ViewUser/>}/> */}
     </Routes>
   );
