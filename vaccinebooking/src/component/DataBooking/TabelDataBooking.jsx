414345de1587e7cbab0c07d7ac1025ec5f6da243
I Gusti Agung Gde Abhirama Adnyana <agungabhirama@gmail.com>
2022-06-13 17:06:35 +0800

    update HiFi

--- vaccinebooking/src/component/DataBooking/TabelDataBooking.jsx
+++ vaccinebooking/src/component/DataBooking/TabelDataBooking.jsx
@@ -4,22 +4,29 @@ import { IconButton } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
 
-const TablePengguna = ({key, nama, nik, jenisVaccine, Number}) => {
+const TablePengguna = ({ key, nama, nik, jenisVaccine, Number }) => {
   return (
-    <div
-      className="d-flex TabelDataBooking justify-content-center"
-      style={{ backgroundcolor: "#D9D9D9" }}
-    >
+    <div className="d-flex TabelDataBooking justify-content-center">
       <div className="col-1">{Number}</div>
       <div className="col-4">{nama}</div>
       <div className="col-2">{nik}</div>
       <div className="col-4">{jenisVaccine}</div>
       <div className="col-1 d-flex justify-content-center">
-        <IconButton data-bs-toggle="tooltip" data-bs-placement="top" title="acc" >
+        <IconButton
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title="acc"
+          color="success"
+        >
           <CheckCircleIcon />
-        </IconButton> 
-        <IconButton data-bs-toggle="tooltip" data-bs-placement="top" title="remove" >
-          <CancelIcon  />
+        </IconButton>
+        <IconButton
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title="remove"
+          color="error"
+        >
+          <CancelIcon />
         </IconButton>
       </div>
     </div>
