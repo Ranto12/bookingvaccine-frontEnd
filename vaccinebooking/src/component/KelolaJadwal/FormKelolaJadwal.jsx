877f46bc9110d44f98453b7533f4abd51244665c
Ranto <rantoirwanda08@gmail.com>
2022-06-24 16:20:11 +0700

    error

--- vaccinebooking/src/component/KelolaJadwal/FormKelolaJadwal.jsx
+++ vaccinebooking/src/component/KelolaJadwal/FormKelolaJadwal.jsx
@@ -78,6 +78,7 @@ const handleSubmit =(e) =>{
       <div >
         <div>
           <label className="mt-4 fw-bold ">Nama Fasilitas Kesehatan</label>
+          <button onSubmit={handleSubmit}>simpan</button>
         </div>
         <input type="text" className="w-100 bg-light input-kelola mt-2 p-1 rounded-2" style={{ border: "1px solid  #D9D9D9" }} value={name}/>
       </div>
@@ -140,7 +141,7 @@ const handleSubmit =(e) =>{
         </span>
       </div>
 
-      <div className="row mt-4">
+      {/* <div className="row mt-4">
         <div className="col-4">
           <h5> Upload Gambar </h5>
           <div className="img-border img-input">
@@ -166,10 +167,38 @@ const handleSubmit =(e) =>{
             <textarea className="p-3 w-100 rounded-3 input-kel-area" disabled value={maps}></textarea>
           </div>
         </div>
-      </div>
-      <div className="text-end mt-3 mb-5">
-          <button className="btn-kelola-jadwal1 me-3  rounded-3 mb-5">Batal</button>
-          <button onClick={handleSubmit}>Simpan</button>
+      </div> */}
+
+      <div className="row mt-4">
+        <div className="col-4">
+          <h5> Upload Gambar </h5>
+          <div className="card img-input">
+            <label>
+              <div className="text-center img-card  ">
+                <BsFileEarmarkImage className="h-50 w-50 " />
+              </div>
+              <input type="file" />
+            </label>
+            <p className="card-text text-center pt-2">
+              Upload Foto Fasilitas Kesehatan Anda Ukuran FOto tidak Lebih dari
+              10 mb
+            </p>
+          </div>
+        </div>
+          <div className="col-8">
+            <div>
+              <h5> Alamat Lengkap </h5>
+              <textarea className="p-3 w-100 rounded-3 input-kel-area" disabled>{address}</textarea>
+            </div>
+            <div>
+              <h6> Link Google Maps </h6>
+              <textarea className="p-3 w-100 rounded-3 input-kel-area" disabled>{maps}</textarea>
+            </div>
+            <div className="text-end mt-3 mb-5">
+                <button className="btn-kelola-jadwal1 me-3  rounded-3 mb-5">Batal</button>
+                <button className="btn-kelola-jadwal ms-3  rounded-3 mb-5" onSubmit={handleSubmit}>Simpan</button>
+            </div>
+          </div>
       </div>
     </div>
   );
