568647f5c8ac5f50d7793e9bc72f5fa98a553106
Ranto <rantoirwanda08@gmail.com>
2022-06-25 13:29:33 +0700

    length admin

--- vaccinebooking/src/component/Dasboard/APIrequired/Length.jsx
+++ vaccinebooking/src/component/Dasboard/APIrequired/Length.jsx
@@ -4,18 +4,16 @@ import axios from "axios"
 // Api
 import api from './../../../API/data/post'
 
-const LengthPenguna = () => {
+export const LengthPenguna = () => {
     const [pengguna, setPengguna]= useState([]);
-
     const Length = pengguna.length;
-    console.log(`pengguna`, Length)
 
     useEffect(()=>{
         // myApi()
         const PenggunaLengthPost = async()=>{
             try{
-                const response = await api.get("/users/roles/ADMIN")
-                setPengguna(response.data)
+                const response = await api.get("/users/roles/USER")
+                setPengguna(response.data.data)
                 ;
             } catch(err){
                 if(err.response){
@@ -39,7 +37,7 @@ const LengthPenguna = () => {
 
 
 
-const LengthAdmin = () => {
+export const LengthAdmin = () => {
     const [datadashboard, setDataDasboard] = useState([]);
     const admin = datadashboard.length;
     // api
@@ -68,4 +66,63 @@ const LengthAdmin = () => {
   )
 }
 
-export default {LengthPenguna, LengthAdmin}
+export const LengthKecamatan = () => {
+    const [datadashboard, setDataDasboard] = useState([]);
+    const area = datadashboard.length;
+    // api
+  // useEffect
+  useEffect(()=>{
+    // myApi()
+    const PenggunaLengthPost = async()=>{
+        try{
+            const response = await api.get("/area")
+            setDataDasboard(response.data.data)
+        } catch(err){
+            if(err.response){
+                //not in the 200 response range
+                console.log(err.response.data)
+                console.log(err.response.status)
+                console.log(err.response.headers)
+            } else{
+                console.log(`Error ${err.message}`)
+            }
+        }
+    }
+    PenggunaLengthPost();
+},[])
+  return (
+    <div>{area}</div>
+  )
+}
+export const LengthRSUD = () => {
+    const [datadashboard, setDataDasboard] = useState([]);
+    const rsud = datadashboard.length;
+
+    console.log(`rsud`, datadashboard)
+    // api
+  // useEffect
+  useEffect(()=>{
+    // myApi()
+    const PenggunaLengthPost = async()=>{
+        try{
+            const response = await api.get("/facility/category/2")
+            setDataDasboard(response.data.data)
+        } catch(err){
+            if(err.response){
+                //not in the 200 response range
+                console.log(err.response.data)
+                console.log(err.response.status)
+                console.log(err.response.headers)
+            } else{
+                console.log(`Error ${err.message}`)
+            }
+        }
+    }
+    PenggunaLengthPost();
+},[])
+  return (
+    <div>{rsud}</div>
+  )
+}
+
+
