d94fcf7763b96b19e919cf0a7e68330c7ed1765a
I Gusti Agung Gde Abhirama Adnyana <agungabhirama@gmail.com>
2022-07-02 00:43:19 +0800

    Handle delete berita

---
+++ vaccinebooking/src/component/HapusDialog/HapusDialogBerita.jsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import { Box, ButtonGroup } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFFFFF",
+    },
+  },
+});
+
+export default function HapusDialogBerita({ open, handleClose, handleSubmit }) {
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <DialogContentText
+            className="d-flex justify-content-center"
+            id="alert-dialog-description"
+          >
+            <Box
+              sx={{
+                fontWeight: "bold",
+                fontSize: "32px",
+                color: "#4E7EA7",
+                textAlign: "center",
+                px: "5vw",
+              }}
+            >
+              Hapus Berita?
+            </Box>
+          </DialogContentText>
+        </DialogContent>
+        <ThemeProvider theme={theme}>
+          <ButtonGroup
+            className="pt-2"
+            color="primary"
+            fullWidth
+            disableElevation
+            variant="outlined"
+            size="large"
+          >
+            <Button
+              onClick={handleClose}
+              sx={{ bgcolor: "#FFFFFF", color: "#4E7EA7", py: "2vh" }}
+            >
+              Kembali
+            </Button>
+            <Button
+              onClick={handleSubmit}
+              sx={{ bgcolor: "#7BD9E8", color: "#4E7EA7", py: "2vh" }}
+            >
+              Konfirmasi
+            </Button>
+          </ButtonGroup>
+        </ThemeProvider>
+      </Dialog>
+    </div>
+  );
+}
--- vaccinebooking/src/component/KelolaBerita/TabelKelolaBerita.jsx
+++ vaccinebooking/src/component/KelolaBerita/TabelKelolaBerita.jsx
@@ -2,47 +2,84 @@ import React from "react";
 import { IconButton } from "@mui/material";
 import { RiPencilFill } from "react-icons/ri";
 import { MdDelete } from "react-icons/md";
-import {useNavigate} from 'react-router-dom';
-import api from '../../API/data/post';
-import moment from 'moment';
+import { useNavigate } from "react-router-dom";
+import api from "../../API/data/post";
+import moment from "moment";
+import HapusDialogBerita from "../HapusDialog/HapusDialogBerita";
 
-const TabelKelolaBerita = ({ Number,id,  title, tanggal, author, key, content}) => {
+const TabelKelolaBerita = ({
+  Number,
+  id,
+  title,
+  tanggal,
+  author,
+  key,
+  content,
+}) => {
   let navigate = useNavigate();
-  const handleNavigate=(e)=>{
+  const handleNavigate = (e) => {
     e.preventDefault();
-    navigate("/EditArtikel", {state:{id : id, judul: title, penulis: author, content: content}});
-  }
+    navigate("/EditArtikel", {
+      state: { id: id, judul: title, penulis: author, content: content },
+    });
+  };
 
-  const handleDetele=(e)=>{
+  const handleDelete = (e) => {
     e.preventDefault();
+    setOpen(false);
     try {
-      api.delete(`/news/${id}`)
-    } catch(err){
-      if (err.response){
-        console.log(err.response.data)
-        console.log(err.response.status)
-        console.log(err.response.headers)
-      } else{
+      api.delete(`/news/${id}`);
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
         console.log(`Error ${err.message}`);
       }
     }
-  }
-  
-  
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="d-flex TabelkelolaBerita justify-content-center TableColor-child">
       <div className="col-1">{Number}</div>
       <div className="col-4">{title}</div>
       <div className="col-2">{author}</div>
-      <div className="col-4">{moment(tanggal).format('DD/MM/YYYY')}</div>
+      <div className="col-4">{moment(tanggal).format("DD/MM/YYYY")}</div>
       <div className="col-1 d-flex justify-content-center PointerClikCss ">
-        <IconButton aria-label="Check" data-bs-toggle="tooltip" data-bs-placement="top" title="edit" className="PointerClikCss">
-          <RiPencilFill onClick={handleNavigate}  />
+        <IconButton
+          aria-label="Check"
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title="edit"
+          className="PointerClikCss"
+        >
+          <RiPencilFill onClick={handleNavigate} />
         </IconButton>
-        <IconButton aria-label="Cancel" data-bs-toggle="tooltip" data-bs-placement="top" title="remove">
-          <MdDelete onClick={handleDetele} />
+        <IconButton
+          aria-label="Cancel"
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title="remove"
+        >
+          <MdDelete onClick={handleClickOpen} />
         </IconButton>
       </div>
+      <HapusDialogBerita
+        open={open}
+        handleClose={handleClose}
+        handleSubmit={handleDelete}
+      ></HapusDialogBerita>
     </div>
   );
 };
