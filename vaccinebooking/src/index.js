f4e0eed0e4ecf1995572c6a7afaf44f054afee6a
Ranto <rantoirwanda08@gmail.com>
2022-07-07 22:38:27 +0700

    mencoba

--- vaccinebooking/src/index.js
+++ vaccinebooking/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import AuthProvider from './Context/AuthProvider';
 
 //styles
 import './index.css';
@@ -9,7 +10,9 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HashRouter>
+      {/* <AuthProvider> */}
       <App />
+      {/* </AuthProvider> */}
     </HashRouter>
 );
 
