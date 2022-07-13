76ec8a0fab0c3857e35566cb2b0428b5495204d5
wandy17 <79315678+wandy17@users.noreply.github.com>
2022-07-13 14:05:22 +0800

    update

---
+++ vaccinebooking/src/component/Basing/Search.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import logo from "../../assets/img/search.png"
+function Search() {
+    return (
+        <div className="d-flex justify-content-center">
+            <img src={logo} alt="" />
+        </div>
+    )
+}
+export default Search
